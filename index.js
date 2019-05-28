3b7bf63775296b29f3f9711ebd053a0bc997f0af
Christoph Gruener <christophgruener@me.com>
2019-05-28 17:05:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,4 +22,10 @@ function BoardMember(name, homeState, training){
 // this.sayHi = function(){
 //   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 
-BoardMember.prototype
+BoardMember.prototype.veto = function(){
+  return 'No, I must disagree'
+}
+
+BoardMember.prototype.approve = function(){
+  return 'You can do that!'
+}
